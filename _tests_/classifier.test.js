82950a04743de731e917c1b062f4269b464e1c06
lidichamp <lidichamp@gmail.com>
2019-01-29 12:11:29 +0100

    all tests passed

--- _tests_/classifier.test.js
+++ _tests_/classifier.test.js
@@ -1,4 +1,4 @@
-const classifier = require('./classifier');
+const classifier = require('../src/classifier');
 const input = [
   {
     name: 'Hendrick',
@@ -81,38 +81,61 @@ const input = [
     regNo: '091',
   },
 ];
-const output = {"group1": 
-{"members": [{"age": 116, "name": "Paul"}, {"age": 117, "name": "Karl"}], "oldest": 117, 
-"regNos": [55, 120], 
-"sum": 233}, 
-"group2": 
-{"members": [{"age": 126, "name": "Louis"}, {"age": 126, "name": "Arthur"}, {"age": 128, "name": "William"}], 
-"oldest": 128, 
-"regNos": [22, 321, 13], 
-"sum": 380}, 
-"group3": 
-{"members": [{"age": 131, "name": "Erwin"}, {"age": 133, "name": "Neils"}, {"age": 135, "name": "Auguste"}], 
-"oldest":135, 
-"regNos": [9, 2, 8], 
-"sum": 399}, 
-"group4": 
-{"members": [{"age": 139, "name": "Albert"}, {"age": 139, "name": "Owen"}], 
-"oldest": 139, 
-"regNos": [33, 52], 
-"sum": 278},
-"group5": {"members": [{"age": 147, "name": "Martin"}, {"age": 150, "name": "Charles"}, 
-{"age": 151, "name": "Marie"}], 
-"oldest": 151, 
-"regNos": [63, 91, 24],
-"sum": 448}, 
-"group6": {"members": [{"age": 152, "name": "Guye"}],
-"oldest": 152, 
-"regNos": [84], 
-"sum": 152},
-"group7": 
- {"members": [{"age": 160, "name": "Max"}, {"age": 165,   "name": "Hendrick"}], 
-"oldest":165, "regNos": [14, 41], "sum": 325}, 
-"noOfGroups": 7}
+const output = 
+{
+  "group1": 
+    {
+      "members": [{"age": 116, "name": "Paul"}, {"age": 117, "name": "Karl"}], 
+      "oldest": 117, 
+      "regNos": [55, 120], 
+      "sum": 233
+    }, 
+  "group2": 
+    {
+      "members": [{"age": 126, "name": "Louis"}, {"age": 126, "name": "Arthur"}, {"age": 128, "name": "William"}], 
+      "oldest": 128, 
+      "regNos": [22, 321, 13], 
+      "sum": 380
+    }, 
+  "group3": 
+    {
+      "members": [{"age": 131, "name": "Erwin"}, {"age": 133, "name": "Neils"}, {"age": 135, "name": "Auguste"}], 
+      "oldest":135, 
+      "regNos": [9, 2, 8], 
+      "sum": 399
+    }, 
+  "group4": 
+    {
+      "members": [{"age": 139, "name": "Albert"}, {"age": 139, "name": "Owen"}], 
+      "oldest": 139, 
+      "regNos": [33, 52], 
+      "sum": 278
+    },
+  "group5": 
+    {
+      "members": [{"age": 147, "name": "Martin"}, {"age": 150, "name": "Charles"},{"age": 151, "name": "Marie"}], 
+      "oldest": 151, 
+      "regNos": [63, 91, 24],
+      "sum": 448
+    }, 
+  "group6": 
+    {
+      "members": [{"age": 152, "name": "Guye"}],
+      "oldest": 152, 
+      "regNos": [84], 
+      "sum": 152
+    },
+  "group7": 
+    {
+      "members": [{"age": 160, "name": "Max"}, {"age": 165,   "name": "Hendrick"}], 
+      "oldest":165, 
+      "regNos": [14, 41], 
+      "sum": 325
+    }, 
+  "noOfGroups": 7
+}
+
 test('classifies students into groups', () => {
   expect(classifier(input)).toEqual(output);
 });
+
